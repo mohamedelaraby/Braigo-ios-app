4b2959d6d6097fc5d538bff5ed6c9bc2c1676bc0
Muhammad Alaraby <mohamedelaraby1995@gmail.com>
2019-09-03 09:06:19 +0200

    Finish product cell

---
+++ Braigo-ios-app/View/ProductCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProductCell.swift
+//  Braigo-ios-app
+//
+//  Created by Winston on 1/3/1441 AH.
+//  Copyright © 1441 AH Winston. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    /*---------------[ MARK:- @IBOutlets ]-----------------------*/
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
